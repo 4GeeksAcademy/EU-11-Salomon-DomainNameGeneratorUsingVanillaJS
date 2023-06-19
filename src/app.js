10d3d161be13ae472c9f779673360fc6b2e7e493
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-06-19 18:43:37 +0000

    logicFinished&DesignEnhanced

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const somebody = ["luke", "vader", "leia", "han"];
+  const action = ["fought", "saved", "beat", "found"];
+  const whatever = ["star", "force", "saber", "droid"];
+  const when = ["wars", "rogue", "clones", "jedi"];
+  const domainEnd = [".com", ".es", ".us", ".io"];
+
+  const domainList = [];
+
+  for (let i = 0; i < somebody.length; i++) {
+    for (let j = 0; j < action.length; j++) {
+      for (let k = 0; k < whatever.length; k++) {
+        for (let l = 0; l < when.length; l++) {
+          for (let m = 0; m < domainEnd.length; m++) {
+            const domainStarWars =
+              somebody[i] + action[j] + whatever[k] + when[l] + domainEnd[m];
+            domainList.push(domainStarWars);
+          }
+        }
+      }
+    }
+  }
+
+  const domainGenerator = document.getElementById("domainGenerator");
+  domainList.forEach(function(domain) {
+    const domainElement = document.createElement("div");
+    domainElement.textContent = domain;
+    domainGenerator.appendChild(domainElement);
+  });
 };
